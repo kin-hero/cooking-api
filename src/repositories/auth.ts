a200a93bdd56769a373620a53cfab003baa80a66
keane putra setiawan <keane@CN23-R-0150.local>
2025-08-10 11:28:06 +0900

    feat: add verifiy email api

--- src/repositories/auth.ts
+++ src/repositories/auth.ts
@@ -1,6 +1,4 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { prisma } from '@/lib/prisma';
 
 export const findUserByEmail = async (email: string) => {
   return await prisma.users_cooking.findUnique({
@@ -25,4 +23,30 @@ export const createUser = async (
     },
   });
   return user;
+};
+
+export const getUserVerificationTokenExpiredAtTime = async (email: string) => {
+  const userVerificationTime = await prisma.users_cooking.findFirst({
+    where: { email: email },
+    select: { verification_token_expired_at: true, email: true },
+  });
+  return {
+    emailFromDB: userVerificationTime?.email,
+    tokenTime: userVerificationTime?.verification_token_expired_at,
+  };
+};
+
+export const verifyUserEmailAddress = async (token: string, email: string) => {
+  const verifiedUser = await prisma.users_cooking.update({
+    where: {
+      verification_token: token,
+      email: email,
+    },
+    data: {
+      email_verified: true,
+      verification_token: null,
+      verification_token_expired_at: null,
+    },
+  });
+  return verifiedUser;
 };
